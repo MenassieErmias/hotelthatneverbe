93f0d958adc500bc959656e11d57a92628d8ae47
Menassie Ermias <ermiasmenase@gmail.com>
2022-12-30 11:03:51 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ hotel/src/Common Components/Header.js
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+import React from 'react';
+import {FaBars, FaTimes} from "react-icons/fa"
+
+export const Navbar = function (){
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("reponsive_nav");
+    }
+
+    return (
+        <header>
+            <h3>logo</h3>
+            <nav ref={ navRef }>
+                <a href="">Home</a>
+                <a href="">Body</a>
+                <a href="">About us</a>
+                <a href="">Contact us</a>
+                <button className= onClick= { showNavbar }>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button onClick= { showNavbar }>
+                <FaBars />
+            </button>
+        </header>
+    )
+}
+
